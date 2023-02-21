6b46c41258bc7c43b8d5d21460f163e6122d3072
williamstravis <travissius@gmail.com>
2023-02-21 09:17:30 -0700

    Added a method for downloading samples of the 30m Copernicus dsm data.

--- windshed/utils.py
+++ windshed/utils.py
@@ -5,13 +5,47 @@ Created on Feb 20th 2022
 
 @author: twillia2
 """
+import json
+import urllib
+
 import ee
+import zipfile
 
 from windshed.main import OUTDIR
 
 ee.Initialize()
 
 
+CO_BBOX = [-105.2961,39.4645,-104.6023,40.01]
+GLO30_PATH = "projects/sat-io/open-datasets/GLO-30"
+
+
 def get_dsm():
     """Get the Copernicus 30m DSM for CONUS."""
+    # Open the 
+    glo30 = ee.ImageCollection(GLO30_PATH)
+    xmin, ymin, xmax, ymax = CO_BBOX
+    region = ee.Geometry.BBox(*CO_BBOX)
 
+    # What's in here
+    i1 = glo30.mosaic()
+
+    # Information methods appear to be broken
+    url = i1.getDownloadURL(
+        {
+            "name": "glo30_dsm_sample",
+            "scale": 30,
+            "region": region,
+            "crs": "epsg:4326"
+        }
+    )
+
+    # Download file
+    dst = OUTDIR.joinpath("glo30_dsm_sample.zip")
+    urllib.request.urlretrieve(url, dst)
+    with zipfile.ZipFile(dst) as zfile:
+        zfile.extractall()
+
+
+if __name__ == "__main__":
+    get_dsm()
